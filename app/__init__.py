84d46e9a4b9e90d8ab50410956dc275b97d7c622
George Yovchev <georgigeorgi73@gmail.com>
2018-05-12 17:39:01 +0300

    Solved bug in __init__ and added address to user table

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,10 @@ login = LoginManager(app)
 
 from app import routes, models, errors
 db.create_all()
-add_point_column_sql="ALTER TABLE `adimatch`.`event` DROP `where`;"
-db.engine.execute(add_point_column_sql)
+try:
+    add_point_column_sql="ALTER TABLE `adimatch`.`event` DROP `where`;"
+    db.engine.execute(add_point_column_sql)
+except Exception:
+    a=0
 add_point_column_sql="ALTER TABLE `adimatch`.`event` ADD COLUMN `where` POINT NOT NULL AFTER `user_id`;"
 db.engine.execute(add_point_column_sql)
--- app/models.py
+++ app/models.py
@@ -23,6 +23,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     phone = db.Column(db.String(32))
+    city = db.Column(db.String(32))
     picture = db.Column(db.String(64))
     events = db.relationship('Event', backref='user')
     sports = db.relationship("Sport",
