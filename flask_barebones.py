f10b36e548f36d475961f0f37bd98ad9cef4a122
Vlado Shumanov <vl.shumanov@gmail.com>
2018-05-12 15:18:13 +0300

    static path

--- flask_barebones.py
+++ flask_barebones.py
@@ -2,7 +2,7 @@ from app import app
 from flask_restful import Api
 from app.api import TestEndpoint, UserProfile
 
-# api = Api(app)
-# api.add_resource(TestEndpoint, '/api/test')
-# api.add_resource(UserProfile,'/user_profile/<int:user_id>')
+api = Api(app)
+api.add_resource(TestEndpoint, '/api/test')
+api.add_resource(UserProfile,'/api/user_profile/<int:user_id>', '/api/user_profile')
 
