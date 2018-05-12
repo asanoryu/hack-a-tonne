54697fce6ce445d3d9e6de6a1cb35451fcf4cab3
Vlado Shumanov <vl.shumanov@gmail.com>
2018-05-12 17:56:09 +0300

    reg and logout client

--- app/api.py
+++ app/api.py
@@ -1,6 +1,6 @@
-from app import api, login
+from app import api, login, db
 from flask_restful import Resource, reqparse
-from app.models import User
+from app.models import User, Sport, association_table_user_sport
 from flask_login import current_user, login_user, logout_user,login_required
 
 
@@ -54,5 +54,57 @@ class CurrentUser(Resource):
 
 class LogoutEndpoint(Resource):
     def get(self):
-        logout_user(current_user)
+        logout_user()
         return {'message' : 'logged out'}
+
+
+class RegisterUserEndpoint(Resource):
+    super(RegisterUserEndpoint, self).__init__()
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('username',
+                                    type=str,
+                                    required=True,
+                                    help='No name provided',
+                                    location='json')
+        self.reqparse.add_argument('password',
+                                    type=str,
+                                    required=True,
+                                    default="",
+                                    help='No parameters provided',
+                                    location='json')
+        self.reqparse.add_argument('email',
+                                    type=str,
+                                    required=True,
+                                    default="",
+                                    help='No parameters provided',
+                                    location='json')
+        self.reqparse.add_argument('sports',
+                                    type=list,
+                                    required=False,
+                                    default="",
+                                    location='json')
+        self.reqparse.add_argument('city',
+                                    type=str,
+                                    location='json')
+    def post(self):
+        args = self.reqparse.parse_args()
+        user = User(username=args['username'],email=args['password'])
+        user.set_password(args['password'])
+        try:
+            user.city = args['city']
+        except KeyError:
+            user.city = None        
+        try:
+            for _sport in args['sports']:
+                sport = Sport.query.filter_by(name=_sport)
+                if not sport:          
+                    sport = Sport(name=_sport)
+                statement = association_table_user_sport.insert().values(user_id=user.id, sport_id=sport.id)
+                db.session.execute(statement)
+                db.session.commit()
+        except KeyError:
+            pass
+        db.session.add(User)
+        db.session.commit()
+
+        return User.to_dict()
