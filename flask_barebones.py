2874a6bf5b24c2150a757d25512419698d760aa6
Vlado Shumanov <vl.shumanov@gmail.com>
2018-05-12 16:15:51 +0300

    login api endpoint

--- flask_barebones.py
+++ flask_barebones.py
@@ -1,8 +1,9 @@
 from app import app
 from flask_restful import Api
-from app.api import TestEndpoint, UserProfile
+from app.api import TestEndpoint, UserProfile, LoginEndpoint
 
 api = Api(app)
 api.add_resource(TestEndpoint, '/api/test')
 api.add_resource(UserProfile,'/api/user_profile/<int:user_id>', '/api/user_profile')
+api.add_resource(LoginEndpoint, '/api/login')
 
