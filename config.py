e6f90b86df3194bda80889d101c4102a68f3b489
Vasil Atanasov <vsatanasov@gmail.com>
2018-05-12 13:54:43 +0300

    adimatch user in db config added

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:12345678@localhost/adimatch'
+    SQLALCHEMY_DATABASE_URI = 'mysql://adimatch:12345678@localhost/adimatch'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
